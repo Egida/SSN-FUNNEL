fb9b1cc0d0ef70f75ba2e71f92f4ee78559fc431
kitty <109695802+CuteCatLover@users.noreply.github.com>
2022-10-12 16:29:43 +0200

    Update licensebypass.go

--- bypass/licensebypass.go
+++ bypass/licensebypass.go
@@ -9,7 +9,7 @@ import (
 
 func ApiHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Licensing request received, bypassed.")
-	w.Write([]byte(fmt.Sprintf("Valid|" + fmt.Sprintf("%d", time.Unix(0, time.Now().Unix()).Add(time.Duration(3000)*24)) + "")))
+	w.Write([]byte(fmt.Sprintf("Valid|" + fmt.Sprintf("%d", time.Unix(0, time.Now().Unix()).Add(time.Duration(3000)*24).Unix()))))
 }
 
 func main() {
